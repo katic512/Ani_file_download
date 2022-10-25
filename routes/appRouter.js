81435dd82cb5c2f4b7f4426be490acb7e3bf17de
katic512 <ssn.karthick@gmail.com>
2022-10-26 00:02:45 +0100

    Update appRouter.js

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const testFolder = "/Users/karthick.narasimhan/github/member-area-root";
+const testFolder =
+  "/Users/karthick.narasimhan/Desktop/Anitha/Node_file_download/node_modules/parseurl";
 // For View
 const homeView = (req, res) => {
-  fs.readdir(testFolder, (err, files) => {
-    console.log(files);
-    res.render("home", { files: files });
-  });
+  var fileDetails = [];
+  res.render("home", { fileDetails: fileDetails });
 };
 
 router.get("/file/:fileName", function (req, res) {
@@ -16,9 +15,49 @@ router.get("/file/:fileName", function (req, res) {
   res.download(file); // Set disposition and send it.
 });
 
-router.post("/fileSearch", function (req, res) {
+router.post("/fileSearch", async (req, res) => {
   console.log(req.body);
+  const searchFileName = req.body.fileName;
+  const fileList = await fs.promises.readdir(testFolder, (err, files) => {
+    return files;
+  });
+  const filterdFileList = fileList.filter(
+    (fileName) => fileName.indexOf(searchFileName) > -1
+  );
+  console.log("printing file list from promise");
+  console.log(filterdFileList);
+
+  var fileDetails = await getFileDetails(filterdFileList);
+  console.log("printing file details");
+  console.log(fileDetails);
+
+  res.json(fileDetails);
 });
+
+router.get("/files", async (req, res) => {
+  console.log(req.body);
+  const fileList = await fs.promises.readdir(testFolder, (err, files) => {
+    //console.log(err);
+    //console.log(files);
+    return files;
+  });
+  console.log("printing file list from promise");
+  console.log(fileList);
+  var fileDetails = await getFileDetails(fileList);
+  console.log("printing file details");
+  console.log(fileDetails);
+  res.json(fileDetails);
+});
+
+const getFileDetails = async function (files) {
+  var fileDetails = [];
+  for (var i = 0; i < 20 && i < files.length; i++) {
+    const stats = await fs.promises.stat(testFolder + "/" + files[i]);
+    var data = { file_name: files[i], modified_date: stats.mtime };
+    fileDetails.push(data);
+  }
+  return fileDetails;
+};
 
 router.get("/home", homeView);
 
